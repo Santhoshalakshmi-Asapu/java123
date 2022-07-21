58e22acebe835d49a8f96325034895f44ab909db
Santhoshalakshmi-Asapu <109283370+Santhoshalakshmi-Asapu@users.noreply.github.com>
2022-07-21 10:06:41 +0530

    Add files via upload

---
+++ BooleanOperation.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Boolean_Operation{
+  public static void main(String[] args){
+    boolean x=true;
+    boolean y=false;
+    System.out.println(x&&y);
+    System.out.println(x||y);
+    System.out.println(!x);
+    System.out.println(!y);
+  }
+}
